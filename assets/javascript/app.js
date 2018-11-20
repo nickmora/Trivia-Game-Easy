6b4c034c196bdffaa1a988b20117b9206907bd96
Nemo <bick.norales@gmail.com>
2018-11-19 22:06:17 -0800

    Everything but showing the incorrect answers works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,40 +5,115 @@ var correct = 0;
 var incorrect = 0;
 var timeUp;
 
-var answerKey = [2, 3, 1, 0, 3, 3, 2, 1, 0, 0];
+var quizEverything = [{   
+        painting: '<img src="assets/images/BlackMesaLandscapeOkeefe.jpg" class = "img artQuestion" alt="Black Mesa Landscape">',
+        answers: [
+            "Pablo Picasshole",
+            "Georgia O'keefe",
+            "Donald Judd",
+            "Leonardo",
+        ],
+        correctAnswer: 1,
+    },
+    
+    {   painting: '<img src="assets/images/AbsintheDegas.jpg" class = "img artQuestion" alt="Absinthe">',
+        answers: [
+            "Claude Monet",
+            "Edouard Manet",
+            "Edgar Degas",
+            "Vincent Van Gogh",
+        ],
+        correctAnswer: 2,
+    },
+    
+    {   painting: '<img src="assets/images/DoubtingThomasCaravaggio.jpg" class = "img artQuestion" alt="Doubting Thomas">',
+        answers: [
+            "Raphael",
+            "Michelangelo Merisi de Caravaggio",
+            "Leonardo",
+            "Jan Van Dyck",
+        ],
+        correctAnswer: 1,
+    },
 
-var questionAsks = [
-    '<img src="assets/images/BlackMesaLandscapeOkeefe.jpg" class = "img artQuestion" alt="Black Mesa Landscape">',
-    '<img src="assets/images/AbsintheDegas.jpeg" class = "img artQuestion" alt="Absinthe">',
-    '<img src="assets/images/DoubtingThomasCaravaggio.jpg" class = "img artQuestion" alt="Doubting Thomas">',
-    '<img src="assets/images/EiffelTowerSeurat.jpg" class = "img artQuestion" alt="Eiffel Tower">',
-    '<img src="assets/images/HolophernesArtemesia.jpg" class = "img artQuestion" alt="Holophernes">',
-    '<img src="assets/images/HouseOfParlimentMonet.jpg" class = "img artQuestion" alt="Parliment">',
-    '<img src="assets/images/Number18Pollock.jpg" class = "img artQuestion" alt="Number 18">',
-    '<img src="assets/images/OrangeYellowRothko.jpg" class = "img artQuestion" alt="Orange & Yellow">',
-    '<img src="assets/images/VivaLaVidaKhalo.jpg" class = "img artQuestion" alt="Watermelons">',
-    '<img src="assets/images/WheatFieldVanGogh.jpeg" class = "img artQuestion" alt="Wheat Field">',
-];
+    {   painting: '<img src="assets/images/EiffelTowerSeurat.jpg" class = "img artQuestion" alt="Eiffel Tower">',
+        answers: [
+            "Georges Seurat",
+            "Edgar Degas",
+            "Claude Monet",
+            "Pablo Picasshole",
+        ],
+        correctAnswer: 0,
+    },
 
-var questionAnswers = [
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-    ["a1", "a2", "a3", "a4"],
-];
+    {   painting: '<img src="assets/images/HolophernesArtemesia.jpg" class = "img artQuestion" alt="Holophernes">',
+        answers: [
+            "Michelangelo Merisi de Caravaggio",
+            "Hieronymous Bosch",
+            "Artemisia Gentileschi",
+            "Clyfford Still",
+        ],
+        correctAnswer: 2,
+    },
+
+    {   painting: '<img src="assets/images/HouseOfParlimentMonet.jpg" class = "img artQuestion" alt="Parliment">',
+        answers: [
+            "Paul Gaugain",
+            "Pierre-Auguste Renoir",
+            "Paul Cezzanne",
+            "Claude Monet",
+        ],
+        correctAnswer: 3,
+    },
+
+    {   painting: '<img src="assets/images/Number18Pollock.jpg" class = "img artQuestion" alt="Number 18">',
+        answers: [
+            "Jackson Pollock",
+            "Louise Bourgeois",
+            "Donald Judd",
+            "Mark Rothko",
+        ],
+        correctAnswer: 0,
+    },
+
+    {   painting: '<img src="assets/images/OrangeYellowRothko.jpg" class = "img artQuestion" alt="Orange & Yellow">',
+        answers: [
+            "Clyfford Still",
+            "Georgia O'keefe",
+            "Jackson Pollock",
+            "Mark Rothko",
+        ],
+        correctAnswer: 3,
+    },
+
+    {   painting: '<img src="assets/images/VivaLaVidaKhalo.jpg" class = "img artQuestion" alt="Watermelons">',
+        answers: [
+            "Diego Rivera",
+            "Jeff Koons",
+            "Frida Khalo",
+            "Gunther Gerzso",
+        ],
+        correctAnswer: 2,
+    },
+
+    {   painting: '<img src="assets/images/WheatFieldVanGogh.jpeg" class = "img artQuestion" alt="Wheat Field">',
+        answers: [
+            "Henri Matisse",
+            "Vincent Van Gogh",
+            "Nick Morales",
+            "Pablo Picasshole",
+        ],
+        correctAnswer: 1,
+    },
+
+]
 
 $('#start').on('click', start);
-$("#submit").on('click', end);
+$(document).on('click', '#submit', end);
 
 function decrement() {
     time --;
-    $("#timerDisplay").html(timeConverter(time));
+    $("#timerDisplay").html("Time Left: " +timeConverter(time));
     if (time === 0 ) {
         timeUp = true;
     }
@@ -54,25 +129,28 @@ function start() {
     $("#quiz").empty();
     generateQuiz();
     time = 90;
-    $("#timerDisplay").append(timeConverter(time));
+    $("#timerDisplay").append("Time Left: " +timeConverter(time));
+    $("#timerDisplay").attr("class", "unhide timer");
     timeUp = false;
 };
 
 function end() {
     clearInterval(intervalId);
     checkAnswers();
+    $("#results").attr("class", "rillrill")
+    $("#results").html("<h4>You got " + correct + " correct out of 10</h4><h5>Scroll up to check your answers</h5>")
 };
 
 function generateQuiz() {
-    for (i = 0; i < questionAsks.length; i ++) {
+    for (i = 0; i < quizEverything.length; i ++) {
         var qaNum = i + 1;
-        var question = "<h3>Question " + qaNum + " : Who painted it?</h3>" + "<div class='imgQues' >" + questionAsks[i] + "</div>";
-        var answers = []
-        questionAnswers[i].forEach(function (ans) {
-            answers.push("<input type='radio' value =" + parseInt(questionAnswers[i].indexOf(ans)) + " name = 'noodle"+qaNum+ "'>" + ans + "<br>")
+        var question = "<h3>Question " + qaNum + " : Who painted it?</h3>" + "<div class='imgQues' >" + quizEverything[i].painting + "</div>";
+        var boop = [];
+        quizEverything[i].answers.forEach(function (ans) {
+            boop.push("<input type='radio' value ='" + parseInt(quizEverything[i].answers.indexOf(ans)) + "'name = 'noodle"+qaNum+ "'>" + ans + "<br>")
         })
-        quizText = question + "<div class = 'answer" + qaNum + "'>" + answers.join(" ") + "</div>";
-        $("#quiz").append("<div class = 'question'>" + quizText + "</div>");
+        quizText = question + "<div class = 'answer" + qaNum + "'>" + boop.join(" ") + "</div>";
+        $("#quiz").append("<div class = 'question' id='strudel>"+qaNum+"'" + quizText + "</div>");
     };
     $("#quiz").append("<button id = 'submit'>Submit</button>");
 };
@@ -101,16 +179,22 @@ function timeConverter(t) {
 
 function checkAnswers() {
     var userSubmit = [];
-    for (var i = 1; i < answerKey.length+1; i ++) {
-        var chigger = $('input[name = noodle' + i + ']:checked').val();
+    for (var i = 0; i < quizEverything.length; i ++) {
+        var poodle = i+1;
+        var chigger = $('input[name = noodle' + poodle + ']:checked').val();
         userSubmit.push(chigger);
+        console.log(chigger)
 
-        if (answerKey[i]=== userSubmit[i]) {
+        if (quizEverything[i].correctAnswer === parseInt(userSubmit[i])) {
             correct ++;
+            $('input[name=noodle'+poodle+']:checked').attr("class", "right");
         }
         else {
             incorrect ++;
+            $('input[name=noodle'+poodle+']:checked').attr("class", "wrong");
+
         }
     }
+
 
 };
